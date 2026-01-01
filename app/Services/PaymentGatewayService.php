950fc75dae09f078361a8b8376c4e7951100946c
Soluciones Inteligentes <sinteligentesw@gmail.com>
2026-01-01 16:28:39 -0400

    modificacion de credenciales de pago facil

--- app/Services/PaymentGatewayService.php
+++ app/Services/PaymentGatewayService.php
@@ -39,8 +39,8 @@ class PaymentGatewayService
         $this->url_query = "https://masterqr.pagofacil.com.bo/api/services/v2/query-transaction";
 
         // Obtener credenciales del .env
-        $this->tokenService = env('PAGO_FACIL_TCTOKEN_SERVICE');
-        $this->tokenSecret = env('PAGO_FACIL_TCTOKEN_SECRET');
+        $this->tokenService = env('PAGO_FACIL_TCTOKEN_SERVICE',"51247fae280c20410824977b0781453df59fad5b23bf2a0d14e884482f91e09078dbe5966e0b970ba696ec4caf9aa5661802935f86717c481f1670e63f35d504a62547a9de71bfc76be2c2ae01039ebcb0f74a96f0f1f56542c8b51ef7a2a6da9ea16f23e52ecc4485b69640297a5ec6a701498d2f0e1b4e7f4b7803bf5c2eba");
+        $this->tokenSecret = env('PAGO_FACIL_TCTOKEN_SECRET',"0C351C6679844041AA31AF9C");
         $this->clientCode = env('PAGO_FACIL_CLIENT_CODE', env('PAGO_FACIL_COMERCE_ID'));
         $this->callbackUrl = env('PAGO_FACIL_CALLBACK_URL', url('/api/payment/callback'));
 
